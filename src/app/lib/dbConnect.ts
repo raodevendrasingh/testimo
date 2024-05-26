9fbc2d42a3b5bf91b4ef63d50bb0801b48bc9bc4
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2024-05-26 22:08:04 +0530

    setup db connection

---
+++ src/app/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+	isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+	if (connection.isConnected) {
+		console.log("Aready connected to databse");
+		return;
+	}
+	try {
+		const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+
+		connection.isConnected = db.connections[0].readyState;
+		console.log("Database Connnected Sucessfully!");
+	} catch (error) {
+		console.log("Database Connnection Failed!", error);
+
+		process.exit(1);
+	}
+}
+
+export default dbConnect;
