0eaa44e59aab93fdf73f225e85e04116261ad2a2
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2024-08-15 18:21:50 +0530

    added user verification api view

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,67 @@
+import { z } from "Zod";
+import dbConnect from "@/lib/dbConnect";
+import { UserModel } from "@/models/User";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+export async function POST(request: Request) {
+	await dbConnect();
+
+	try {
+		const { username, code } = await request.json();
+		const decodedUsername = decodeURIComponent(username);
+
+		const user = await UserModel.findOne({ username: decodedUsername });
+
+		if (!user) {
+			return Response.json(
+				{
+					sucess: false,
+					message: "User not found",
+				},
+				{ status: 404 }
+			);
+		}
+
+		const isCodeValid = user.verifyCode === code;
+		const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+		if (isCodeValid && isCodeNotExpired) {
+			user.isVerified = true;
+			await user.save();
+
+			return Response.json(
+				{
+					sucess: true,
+					message: "User verified successfully",
+				},
+				{ status: 200 }
+			);
+		} else if (!isCodeValid) {
+			return Response.json(
+				{
+					sucess: false,
+					message: "Incorrect verification code",
+				},
+				{ status: 400 }
+			);
+		} else {
+			return Response.json(
+				{
+					sucess: false,
+					message:
+						"Verification code is expired. Please signup again to receive a new code!",
+				},
+				{ status: 400 }
+			);
+		}
+	} catch (error) {
+		console.error("Error verifying code!\n", error);
+		return Response.json(
+			{
+				sucess: false,
+				message: "Error verifying code",
+			},
+			{ status: 500 }
+		);
+	}
+}
