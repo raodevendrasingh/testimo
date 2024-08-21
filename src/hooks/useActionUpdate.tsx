94f663de8114ce64c98d76ec33e228579498468e
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2024-08-21 15:39:37 +0530

    Feat(core): Added custom hook for action update

---
+++ src/hooks/useActionUpdate.tsx
@@ -0,0 +1,33 @@
+import { ApiResponse } from "@/types/ApiResponse";
+import axios, { AxiosError } from "axios";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export const useActionUpdate = () => {
+	const [loading, setLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string | null>(null);
+
+	const actionUpdate = async (feedbackId: string, action: string) => {
+		setLoading(true);
+		setError(null);
+
+		try {
+			const response = await axios.post<ApiResponse>("/api/update-action", {
+				feedbackId,
+				action,
+			});
+			toast.success(response.data.message);
+		} catch (error) {
+			const axiosError = error as AxiosError<ApiResponse>;
+			toast.error("Error", {
+				description:
+					axiosError.response?.data.message ||
+					"Failed to update feedbak action!",
+			});
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return { actionUpdate, loading, error };
+};
